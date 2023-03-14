b850d45b7a257af6aa91e5315c5713b29d73e995
Jacquelyn <125713386+professrx@users.noreply.github.com>
2023-03-14 09:29:25 -0400

    adjusted split deck method, added psuedocode

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ class Deck {
     }
 
     createDeck() {
-        let suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
-        let ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+        const suits = ["♠", "♡", "♢", "♣"]
+        const ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
         for (let i = 0; i < suits.length; i++) {
             for (let j = 0; j < ranks.length; j++) {
@@ -39,14 +39,9 @@ class GameOfWar {
     ///each player gets 26 cards (half a deck) to start
     init() {
         let deck = new Deck()
-        const splitDeck = Math.ceil(deck.cards.length / 2)
-        let halfDeck = deck.cards.splice(0, splitDeck)
-    ///assign each player half of the card deck
-        this.p1.push(...halfDeck)
-        this.p2.push(...deck.cards)
-
+        this.p1 = deck.cards.splice(0, deck.cards.length / 2)
+        this.p2 = deck.cards
     }
-/// initialize the game by taking one card from each players hand 
 
     battle() {
         // Grab the top card from each player's hand
@@ -55,19 +50,20 @@ class GameOfWar {
         /// card values from each players hand are compared and the highest card value wins the cards from that round (add both drawn cards to winner's deck). 
         if (p1Card.val > p2Card.val) {
             console.log("Player 1 wins round!")
-            // What do I do after player 1 wins?
+        /// player 1 wins both cards
             this.p1.push(p2Card, p1Card)
             console.log(this.p1.length + "deck length 1")
             console.log(this.p2.length + "deck length 2")
             console.log(this.pile.length  + "pile")
+        /// player 2 wins both cards
         } else if (p2Card.val > p1Card.val) {
             console.log("Player 2 wins round!")
             this.p2.push(p2Card, p1Card)
             console.log(this.p1.length + "deck length 1")
             console.log(this.p2.length + "deck length 2")
             console.log(this.pile.length  + "pile")
+        /// if there's a tie, war begins. 
         } else {
-        // if there's a tie, war begins. 
             console.log("Time for war!")
             this.pile.push(p2Card, p1Card)
             this.war()
@@ -75,12 +71,13 @@ class GameOfWar {
       }
 
     war() {
-        // add 3 cards from each player to pile
-        let warPile = []
+        /// 
+        let warPile = this.pile
         let p1Card = this.p1.pop()
         let p2Card = this.p2.pop()
+        /// add 3 cards from each player to pile
         warPile.push(...this.p1.slice(0, 3), ...this.p2.slice(0, 3))
-        this.pile.push(warPile)
+        warPile.push(cards)
         console.log(this.pile.length + 'big number')
 
         // during war, the game is initialized again- highest value wins all the cards
